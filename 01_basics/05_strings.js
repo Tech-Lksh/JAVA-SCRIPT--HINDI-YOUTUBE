52da38c12022b92f2cd5edaaed15ee9a03e66f2e
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-04-09 12:58:17 +0000

    js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -16,15 +16,15 @@ const gameName = new String('hitesh-hc-com')
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0, 4) // Negative value nahi de sakte
+const newString = gameName.substring(0, 4) // Substring Negative value nahi de sakte
 console.log(newString);
  
-const anotherString = gameName.slice(-8, 4) // Negative value de sakte hai
+const anotherString = gameName.slice(-8, 4) // Slice Negative value de sakte hai
 console.log(anotherString);
 
 const newStringOne = "   hitesh    "
 console.log(newStringOne);
-console.log(newStringOne.trim()); // Remove starting space and ending space
+console.log(newStringOne.trim()); // Trim Remove starting space and ending space
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
